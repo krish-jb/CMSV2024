3f1406164b925ab8bd884a1a1c76efca4318fa4c
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-11-06 10:02:34 +0530

    added doctor

--- TEAM3/cmsapiproj/apibackendapp/serializers.py
+++ TEAM3/cmsapiproj/apibackendapp/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
-from .models import Doctor
+from .models import MedicinePrescription, LabTestPrescription, Doctor
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model=Doctor
         fields='__all__'
 
-        
+class MedicinePrescriptionSerializer(serializers.ModelSerializer):
+    FREQUENCY_CHOICES = [("once_a_day", "Once a day"), ("twice_a_day", "Twice a day")]
+
+    Frequency = serializers.ChoiceField(choices=FREQUENCY_CHOICES)
+
+    class Meta:
+        model = MedicinePrescription
+        fields = (
+            "AppointmentId",
+            "MedicineId",
+            "Dosage",
+            "Frequency",
+        )
+
+
+class LabTestPrescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LabTestPrescription
+        fields = ("AppointmentId", "LabTestId", "LabTestValue", "Remarks")
